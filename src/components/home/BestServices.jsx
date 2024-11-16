cfc5ef543796670b42aff848615dcdc321d906ca
ivanexist <ivanexist@gmail.com>
2024-11-16 14:05:20 +0700

    akhirnya bisaaaa

--- src/components/home/BestServices.jsx
+++ src/components/home/BestServices.jsx
@@ -6,6 +6,7 @@ import { GCSContext } from "../../context/GCSContext";
 
 export default function BestServices() {
   const { scrollToTop, gcsData } = useContext(GCSContext);
+  const ArraySlice3 = gcsData.Services || [];
   // const scrollToTop = () => {
   //   window.scrollTo({ top: 0, behavior: "auto" });
   // };
@@ -22,27 +23,28 @@ export default function BestServices() {
         <div>
           <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-8">
             {/* Best Quality */}
-            {gcsData.Services.slice(0, 3).map((service) => (
-              <Link
-                key={service.service_id}
-                to={`/services/${service.service_id}`}
-                onClick={scrollToTop}
-                className="flex flex-col"
-              >
-                <div className="flex justify-center items-center">
-                  <img
-                    src={`https://raw.githubusercontent.com/ivanexist/gcs-new/refs/heads/master/public/images/${service.service_image}`}
-                    alt="Street constructions"
-                    className="w-96 h-64"
-                  />
-                </div>
-                {/* {console.log(service)} */}
-                <div>
-                  <p className="font-semibold my-4 text-xl text-blue-600">
-                    {service.service_name}
-                  </p>
-                </div>
-              </Link>
+            {/* {console.log(ArraySlice3.slice(0, 3))} */}
+            {ArraySlice3.slice(0, 3).map((service) => (
+              <div key={service.service_id} className="flex flex-col">
+                <Link
+                  to={`/services/${service.service_id}`}
+                  onClick={scrollToTop}
+                >
+                  <div className="flex justify-center items-center">
+                    <img
+                      src={`https://raw.githubusercontent.com/ivanexist/gcs-new/refs/heads/master/public/images/${service.service_image}`}
+                      alt="Street constructions"
+                      className="w-96 h-64"
+                    />
+                  </div>
+                  {console.log(service)}
+                  <div>
+                    <p className="font-semibold my-4 text-xl text-blue-600">
+                      {service.service_name}
+                    </p>
+                  </div>
+                </Link>
+              </div>
             ))}
             {/* <div>
                 <p className="text-gray-500 text-justify">
